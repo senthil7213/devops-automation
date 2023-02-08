986beb0296af39582101942b5627cbf0273c0ddb
skkumar <senthiil7213@gmail.com>
2023-02-08 22:31:12 +0530

    devops project

---
+++ src/main/java/com/devops/devopsintegration/DevopsintegrationApplication.java
@@ -0,0 +1,21 @@
+package com.devops.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsintegrationApplication {
+
+	@GetMapping("/devops")
+	public String getMessage() {
+		return "devops Integration";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsintegrationApplication.class, args);
+	}
+
+}
